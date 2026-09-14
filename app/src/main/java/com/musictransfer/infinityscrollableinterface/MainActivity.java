5b534b384b8db578eae2ba4481059992788cafd5
Timéo <titipas2009@gmail.com>
2026-09-14 18:25:28 +0200

    Create the list of citation for the future test before add the database

--- app/src/main/java/com/musictransfer/infinityscrollableinterface/MainActivity.java
+++ app/src/main/java/com/musictransfer/infinityscrollableinterface/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        List<Citation> citationList = new ArrayList<Citation>();
+        Citation c1 = new Citation("Je pense donc je suis", "Descartes");
+        Citation c2 = new Citation("L'homme est un être fini", "Blaise Pascal");
+        Citation c3 = new Citation("Test3", "Unknown");
+        Citation c4 = new Citation("Ah", "unknown");
+        Citation c5 = new Citation("Un petit pas pour l'homme un grand pas pour l'humanité", "Neil Armstrong");
+
+        citationList.add(c1);
+        citationList.add(c2);
+        citationList.add(c3);
+        citationList.add(c4);
+        citationList.add(c5);
     }
 }
